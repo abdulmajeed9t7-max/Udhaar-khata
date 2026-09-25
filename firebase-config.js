f4169e927c29d6fbe4191ec6cdb0fb39f0099f67
abdulmajeed9t7-max <abdulmajeed9t7@gmail.com>
2026-09-25 17:04:52 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxT2PO8dksz_4DdbTqH5ucjKz9W6w4Oo",
+  authDomain: "udhaar-ktkstore.firebaseapp.com",
+  projectId: "udhaar-ktkstore",
+  storageBucket: "udhaar-ktkstore.firebasestorage.app",
+  messagingSenderId: "1096603737191",
+  appId: "1:1096603737191:web:3f1c9fc0ca1b1d2ff088c2"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
