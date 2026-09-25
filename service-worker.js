cbbfbe02e6d5b68c3bc73382132a7dd4498cb5bb
abdulmajeed9t7-max <abdulmajeed9t7@gmail.com>
2026-09-25 23:55:27 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'udhaar-khata-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './firebase-config.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        ASSETS.map((url) =>
+          cache.add(url).catch((err) => console.log('Cache fail:', url, err))
+        )
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // صرف اپنی سائٹ کی فائلیں کیش کریں، Firebase کو نہیں
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  }
+});
